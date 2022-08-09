e20a4bb07899e9e6b2a3998d2d6ff4fbe6038893
JUstplstry <rodeantavgolding@gmail.com>
2022-08-09 17:15:59 -0400

    done

--- firebase.js
+++ firebase.js
@@ -9,7 +9,14 @@ import {
 
 // Your web app's Firebase configuration
 // ENTER FIREBASE CONFIG HERE
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCgbNnfuRdlc4c7Yu077ZnUpDXvjltQY34",
+  authDomain: "lolz-22764.firebaseapp.com",
+  projectId: "lolz-22764",
+  storageBucket: "lolz-22764.appspot.com",
+  messagingSenderId: "55769011222",
+  appId: "1:55769011222:web:f6bf7f38a8aca9f0a25137"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 import { ref, push, set, database, onValue } from "./firebase.js";
 
 //get elements from index.html
-
+let itemInput = document.getElementById("itemInput")
+let submitBtn = document.getElementById("submitBtn")
+let itemContainer = document.getElementById("itemsContainer")
 //refrence a part of our database
-
+let itemsRef = ref(database,"items")
 //add item to database
+submitBtn.onclick = function(event){
+	event.preventDefault()
+	let itemInputVal = itemInput.value
+	console.log(itemInputVal)
+	itemInput.value = ""
 
+	let newItemRef = push(itemsRef)// new place to add items in section items where can place our data
+
+	set(newItemRef,itemInputVal)
+
+}
 //show items on firebase in our items container
+onValue(itemsRef,(snapshot)=>{
+	itemContainer.innerHTML = " "
+	let data = snapshot.val()
+	console.log(snapshot.value)
+
+	for(const property in data){
+		console.log(data[property])
+		let newLi = document.createElement("li")
+		newLi.innerHTML = data[property]
+		itemContainer.append(newLi)
+	}
+})
